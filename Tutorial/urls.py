f73e317c7eb1aea880221c1304a598bcfcf25f56
Andrea Kramm <krammandrea@gmail.com>
2014-03-08 17:38:18 -0800

    Added urls and views

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+# /polls/index/3 hits here first
+# it matches the first url, which then calls polls.urls with shortend /index/3
---
+++ polls/urls.py
@@ -0,0 +1,41 @@
+from django.conf.urls import patterns, url
+
+from polls import views
+
+urlpatterns = patterns('',
+    # url(r'^$', views. index, name='index'),
+    url(r'^index', views.index, name='index'),
+    url(r'^twoInd', views.index2, name='indexTwo'),
+    # url(r'?P<>')
+)
+
+#TODO do urls for detail, result, vote
+
+"""
+If your view method takes more than the "request" parameter you
+can assign values to the other parameters using "kwargs" as a dictionary,
+where the parameter is a "key" and the value is the "key:value".
+
+For example:
+
+In views.py
+def sample(request,foo):
+    return HttpResponse("Hello %d" % foo)
+
+In urls.py in your app
+urlpatterns = patterns('',
+    url(r'^$', views.sample, name='sample', kwargs = {"foo":1}))
+
+"""
+
+
+
+"""
+Regular Expressions:
+start with r''
+^string1 : the input has to start with string1
+string1$ : the input has to end with string1
+\d+ : the input can be multiple digits
+?P<useThisVariableLater>\d+ : any digits given in input
+                 will be saved in useThisVariableLater
+"""
